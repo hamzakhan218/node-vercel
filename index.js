14ab1ee0624cb2749e399898fa5e13e5518839a7
Hamza Bashir <hamzabashir218@gmail.com>
2023-06-26 17:55:09 +0500

    initial

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-// Import packages
+
 const express = require("express");
 const home = require("./routes/home");
 
-// Middlewares
 const app = express();
 app.use(express.json());
 
-// Routes
 app.use("/home", home);
 
 // connection
